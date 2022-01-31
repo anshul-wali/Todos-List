58a91153ac53c6be44ddf9606c2ff62023c8c321
anshul-wali <anshulwali2001@gmail.com>
2022-01-31 19:17:19 +0530

    init

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,5 +1,13 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-export default function Todos() {
-  return <div>todos</div>;
-}
+export const Todos = (props) => {
+  return (
+    <div className="container">
+      <h3 className="my-3">Todos list</h3>
+      {props.todos.map((todo) => {
+        return <TodoItem todo={todo} onDelete={onDelete} />;
+      })}
+    </div>
+  );
+};
