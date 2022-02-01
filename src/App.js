b28a1433e4df813040a7ef76ef9d54bc48cb790c
anshul-wali <anshulwali2001@gmail.com>
2022-02-01 23:15:06 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import "./App.css";
 import Header from "./MyComponents/Header";
 import { Todos } from "./MyComponents/Todos";
 import { Footer } from "./MyComponents/Footer";
-
+import React, { useState } from "react";
 function App() {
-  let todos = [
-    { Sno: 1, title: "go to the market", desc: "get this job done1" },
-    { Sno: 2, title: "go to the ghat", desc: "get this job done2" },
-    { Sno: 3, title: "go to the mall", desc: "get this job done3" },
-  ];
+  const onDelete = (todo) => {
+    // console.log("i am ondelete todo", todo);
+
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+  };
+
+  const [todos, setTodos] = useState([
+    { sno: 1, title: "go to the market", desc: "get this job done1" },
+    { sno: 2, title: "go to the ghat", desc: "get this job done2" },
+    { sno: 3, title: "go to the mall", desc: "get this job done3" },
+  ]);
   return (
     <div className="App">
       <Header title="My Todos List" searchBar={false} />
-      <Todos todos={todos} />
+      <Todos todos={todos} onDelete={onDelete} />
       <Footer />
     </div>
   );
